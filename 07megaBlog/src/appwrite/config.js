7a23b13750420806db053c725e841a1f41d14b81
12Uttam <12uttamdarekar@gmail.com>
2024-02-13 16:52:06 +0530

    creation of config file for service

---
+++ 07megaBlog/src/appwrite/config.js
@@ -0,0 +1,19 @@
+import conf from "../conf/conf";
+import { Client, Account, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+}
+
+const service = new Service();
+export default service;
