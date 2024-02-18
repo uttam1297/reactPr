eea4908d6878ba7aaec53201c1af86a2e6fddd25
12Uttam <12uttamdarekar@gmail.com>
2024-02-18 23:15:06 +0530

    added RTE

--- 07megaBlog/src/components/index.js
+++ 07megaBlog/src/components/index.js
@@ -5,4 +5,17 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
 import Button from "./Button";
-export { Header, Footer, Container, Logo, LogoutBtn, Input, Button };
+import Select from "./Select";
+import RTE from "./RTE";
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Input,
+  Button,
+  Select,
+  RTE,
+};
