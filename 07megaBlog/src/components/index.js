2c00b8df7873b0ce9f3ac3beb42da8233793d829
12Uttam <12uttamdarekar@gmail.com>
2024-02-18 19:39:52 +0530

    components added login, postcard and auth service added

--- 07megaBlog/src/components/index.js
+++ 07megaBlog/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-
-export { Header, Footer, Container, Logo, LogoutBtn };
+import Input from "./Input";
+import Button from "./Button";
+export { Header, Footer, Container, Logo, LogoutBtn, Input, Button };
